1f5d51d8b0eb120d8c1642e611de6e1447a767f3
Ostroverkhov Andrey <ost4nd@yandex.ru>
2020-11-13 14:02:35 +0300

    Refactor code

--- src/ru/vsu/cs/ostroverkhov/Main.java
+++ src/ru/vsu/cs/ostroverkhov/Main.java
@@ -6,42 +6,19 @@ public class Main {
 
     public static void main(String[] args) {
         int s = readS();
-        if (stopMistake(s)) {
+        if (s >= 2 && s % 2 == 0) {
             printRhombus(s);
+        } else {
+            printErrorCheck();
         }
     }
 
-    public static int readS() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter S: ");
-        return scanner.nextInt();
+    static void printRhombus(int s) {
+        printTopHalfOfRhombus(s);
+        printLowerHalfOfRhombus(s);
     }
 
-    public static boolean stopMistake(int s) {
-        boolean result = true;
-        if (s < 2 || s % 2 != 0) {
-            result = false;
-            System.out.println("Error! S must be more than or equal to 2 and be even");
-        }
-        return result;
-    }
-
-    public static void printTopHalfOfRhombus(int s) {
-        for (int i = 1; i <= s / 2; i++) {
-            for (int j = s / 2 - i; j > 0; j--) {
-                System.out.print(" ");
-            }
-            for (int j = 1; j <= i; j++) {
-                System.out.print("/");
-            }
-            for (int j = 1; j <= i; j++) {
-                System.out.print("\\");
-            }
-            System.out.println();
-        }
-    }
-
-    public static void printLowerHalfOfRhombus(int s) {
+    static void printLowerHalfOfRhombus(int s) {
         for (int i = 1; i <= s / 2; i++) {
             for (int j = 1; j < i; j++) {
                 System.out.print(" ");
@@ -56,8 +33,28 @@ public class Main {
         }
     }
 
-    public static void printRhombus(int s) {
-        printTopHalfOfRhombus(s);
-        printLowerHalfOfRhombus(s);
+    static void printTopHalfOfRhombus(int s) {
+        for (int i = 1; i <= s / 2; i++) {
+            for (int j = s / 2 - i; j > 0; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("/");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("\\");
+            }
+            System.out.println();
+        }
+    }
+
+    static void printErrorCheck() {
+        System.out.println("Error! S must be more than or equal to 2 and be even");
+    }
+
+    static int readS() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter S: ");
+        return scanner.nextInt();
     }
 }
